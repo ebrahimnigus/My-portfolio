c4740db975bb2c6e529386cddd3e85ab01a2c1c1
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-07 15:39:50 +0300

    update the profession

--- src/Components/Home/Social.jsx
+++ src/Components/Home/Social.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/ebrahimibnmalik" className="home__social-icon" target="_blank">
-            <i class="uil uil-instagram"></i>
+        <a href="https://www.instagram.com/ebrahimibnmalik" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+            <i className="uil uil-instagram"></i>
         </a>
-        <a href="http://t.me/Ebraeo" className="home__social-icon" target="_blank">
-        <i class="uil uil-telegram-alt"></i>      
+        <a href="http://t.me/Ebraeo" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+            <i className="uil uil-telegram-alt"></i>      
         </a>
-        <a href="https://github.com/ebrahimnigus" className="home__social-icon" target="_blank">
-        <i class="uil uil-github-alt"></i>
+        <a href="https://github.com/ebrahimnigus" className="home__social-icon" target="_blank" rel="noopener noreferrer">
+            <i className="uil uil-github-alt"></i>
         </a>
     </div>
   )
