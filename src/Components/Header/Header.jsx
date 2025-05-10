b1f23bab6ce77e125ed2e8c8088d2f8f7bc47469
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-05-10 22:40:14 +0300

    update responsiveness

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">{"{Ebrahim}"}</a>
+        <a href="index.html" className="nav__logo home__name">{"{Ebrahim}"}</a>
 
         <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
           <ul className="nav__list nav_list">
